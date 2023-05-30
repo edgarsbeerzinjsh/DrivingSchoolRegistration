292367cc7b994c1c2343a262ff452c0bc3e595a2
edgars.beerzinjsh <103140210+edgarsbeerzinjsh@users.noreply.github.com>
2023-05-30 10:34:56 +0300

    Added DbContext

---
+++ API/DrivingSchool.Data/DrivingSchoolDbContext.cs
@@ -0,0 +1,13 @@
+﻿using DrivingSchool.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DrivingSchool.Data
+{
+    public class DrivingSchoolDbContext : DbContext, IDrivingSchoolDbContext
+    {
+        public DrivingSchoolDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ API/DrivingSchool.Data/IDrivingSchoolDbContext.cs
@@ -0,0 +1,11 @@
+﻿using DrivingSchool.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DrivingSchool.Data
+{
+    public interface IDrivingSchoolDbContext
+    {
+        DbSet<Student> Students { get; set; }
+        int SaveChanges();
+    }
+}
