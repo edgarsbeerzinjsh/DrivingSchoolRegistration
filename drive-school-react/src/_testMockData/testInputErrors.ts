795eb171b0734c0f47b4b88fdd8e9b6b001c63b4
edgars.beerzinjsh <103140210+edgarsbeerzinjsh@users.noreply.github.com>
2023-06-02 15:47:15 +0300

    React helper function tests added

---
+++ drive-school-react/src/_testMockData/testInputErrors.ts
@@ -0,0 +1,11 @@
+export const testInputErrors = { 
+    firstName: "Z",
+    lastName: "Z",
+    yearOfBirth: "Z",
+    email: "Z",
+    mobilePhone: "Z",
+    address: "Z",
+    city: "Z",
+    trainingCategory: "Z",
+    examTime: "Z"
+}
